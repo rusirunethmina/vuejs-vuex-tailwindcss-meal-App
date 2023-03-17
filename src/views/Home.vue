66b7461674d47df348b5f8544d8f6052095e98f0
Rusiru1998 <rusiruc21@gmail.com>
2023-03-17 13:14:04 +0530

    inital commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search For Meals"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link to="/" v-for="letter in letters">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+</script>
